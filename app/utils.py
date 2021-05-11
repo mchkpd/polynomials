1f3c2cb5fd050b882efbafdd9974b9fdc59ea007
Michał Kopydłowski <m.kopydlowski@merixstudio.com>
2021-05-11 15:23:33 +0200

    Update comment.

--- app/utils.py
+++ app/utils.py
@@ -9,7 +9,7 @@ def eval_poly(expression: str, x: float, y: float):
     # Add coefficient "1" when not specified explicitly in the middle of the expression.
     prepared_expression = re.sub('(\\+(x|y))', r'+1\2', prepared_expression)
     prepared_expression = re.sub('(-(x|y))', r'-1\2', prepared_expression)
-    # Add proper mathematical operations.
+    # Add proper mathematical operations (ASCII codes: 94 => ^, 120 => x, 121 => y).
     prepared_expression = prepared_expression.translate({94: '**', 120: '*x', 121: '*y'})
     # Interpolate values.
     prepared_expression = prepared_expression.translate({120: str(x), 121: str(y)})
